696b4ce3fb1ce18c5215009776050bc9d2f57e01
Andre456 <BELUY@yandex.ru>
2019-10-17 21:34:46 +0300

    result

--- src/by/belhard/j18/lesson_class/lesson3/Class2.java
+++ src/by/belhard/j18/lesson_class/lesson3/Class2.java
@@ -6,6 +6,19 @@ public class Class2 {
         char c  = (char) a;
         System.out.println((char) (c - 26));
         System.out.println((char)(45));
+int [] ints = {1, 2,5, -2, 10};
+//for (int i = 0; i < ints.length; i++);
+//    System.out.println(ints[i]);
+//for (int i = ints)
+        int[][] arrayD = {{1,2,3,4,5},{4,3}};
+        for (int i = 0; i <arrayD.length; i++){
+        for (int j =0; j < arrayD.length; j++) {
+            System.out.println(arrayD[i][j] + " ");
+        }
+            System.out.println();
+
+        }
+        System.out.println();
 
     }
 }
